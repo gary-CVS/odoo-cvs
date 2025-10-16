0c59298a273cd97e97a367e8ad94ea8027495aed
gary-CVS <gary@cvs-pro.fr>
2025-10-16 22:29:47 +0200

    Create __manifest__.py

---
+++ addons/cvs_units/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "CVS Units - Poids en grammes",
+    "version": "19.0.1.0.0",
+    "author": "CVS (Comptoir Vis Spéciale)",
+    "website": "https://cvs-pro.fr",
+    "category": "Stock",
+    "summary": "Utiliser le gramme comme unité de poids par défaut et afficher Poids (g)",
+    "depends": ["base", "product", "stock"],
+    "data": [
+        "views/product_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
